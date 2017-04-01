1649b58b67815582d7bf33c7dece8846cd99e8d7
Spacehug <spacehug.o0@gmail.com>
2017-04-01 10:20:40 +0500

    Minor tweaks
    Used dictionary instead of a list
    Added parentheses near -((a - r) // -b)

--- pyborgeous.py
+++ pyborgeous.py
@@ -7,10 +7,13 @@ import random
 
 class Page:
 
-    # [Pages per book, books per shelf, shelves per bookcase, bookcases per room]
-    library_configuration = [410, 32, 5, 4, 0]
-    # [Characters per page, characters per title]
-    page_configuration = [3200, 25]
+    library_configuration = {'pages_per_book': 410,
+                             'books_per_shelf': 32,
+                             'shelves_per_bookcase': 5,
+                             'bookcases_per_room': 4,
+                             'room': 0}
+    page_configuration = {'characters_per_page': 3200,
+                          'characters_per_title': 25}
 
     def __init__(self, encode_string, address, text):
         self.encode_string = encode_string
@@ -30,44 +33,44 @@ class Page:
     def get_page_text_by_address(self):
         room, bookcase, shelf, book, page_number = self.address.split('\t')
         magic_number = helpers.base_to_int(room, self.encode_string)
-        magic_number = magic_number * self.library_configuration[3] + int(bookcase)
-        magic_number = magic_number * self.library_configuration[2] + int(shelf)
-        magic_number = magic_number * self.library_configuration[1] + int(book)
-        magic_number = magic_number * self.library_configuration[0] + int(page_number)
+        magic_number = magic_number * self.library_configuration['bookcases_per_room'] + int(bookcase)
+        magic_number = magic_number * self.library_configuration['shelves_per_bookcase'] + int(shelf)
+        magic_number = magic_number * self.library_configuration['books_per_shelf'] + int(book)
+        magic_number = magic_number * self.library_configuration['pages_per_book'] + int(page_number)
         self.text = helpers.int_to_string(magic_number)
         return self.text
 
     def get_address_by_page_text(self):
         space = ' '
-        if len(self.text) < self.page_configuration[0]:
-            self.text += space * (self.page_configuration[0] - len(self.text))
-        elif len(self.text) > self.page_configuration[0]:
-            self.text = self.text[:self.page_configuration[0]]
+        if len(self.text) < self.page_configuration['characters_per_page']:
+            self.text += space * (self.page_configuration['characters_per_page'] - len(self.text))
+        elif len(self.text) > self.page_configuration['characters_per_page']:
+            self.text = self.text[:self.page_configuration['characters_per_page']]
         magic_number = helpers.string_to_int(self.text)
         self.address = '\t'.join(self.coordinates(magic_number))
         return self.address
 
     def get_address_by_page_text_random(self):
-        if len(self.text) < self.page_configuration[0]:
-            postfix_range = random.randrange(self.page_configuration[0] - len(self.text) - 1)
-            prefix_range = self.page_configuration[0] - len(self.text) - postfix_range
+        if len(self.text) < self.page_configuration['characters_per_page']:
+            postfix_range = random.randrange(self.page_configuration['characters_per_page'] - len(self.text) - 1)
+            prefix_range = self.page_configuration['characters_per_page'] - len(self.text) - postfix_range
             prefix = ''.join(random.choice(self.encode_string) for a in range(prefix_range))
             postfix = ''.join(random.choice(self.encode_string) for b in range(postfix_range))
             self.text = prefix + self.text + postfix
-        elif len(self.text) > self.page_configuration[0]:
-            self.text = self.text[:self.page_configuration[0]]
+        elif len(self.text) > self.page_configuration['characters_per_page']:
+            self.text = self.text[:self.page_configuration['characters_per_page']]
         magic_number = helpers.string_to_int(self.text)
         self.address = '\t'.join(self.coordinates(magic_number))
         return self.address
 
     def coordinates(self, magic_number):
         address = []
-        for value in self.library_configuration:
+        for key, value in self.library_configuration.items():
             if value == 0:
                 address.append(helpers.int_to_base(magic_number, self.encode_string))
             else:
                 result_value = magic_number % value
-                magic_number = - (magic_number - result_value) // - value
+                magic_number = - ((magic_number - result_value) // - value)
                 address.append(str(result_value))
         address.reverse()
         return address
